6229b9ea0850213aa09cf56c73348ff41883469c
Térence <terencehecq@gmail.com>
2020-03-17 17:00:32 +0100

    initialisations

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cinemaAngularJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cinemaAngularJsApp
+ */
+angular.module('cinemaAngularJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
