babb4228562854fcbd694a9f68abc97fb2f42217
Térence <terencehecq@gmail.com>
2020-03-18 09:16:40 +0100

    reinitialisation ->bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2020-03-17 using generator-angular 0.16.0
+// Generated on 2020-03-18 using generator-angular 0.16.0
 'use strict';
 
 // # Globbing
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,6 @@
  */
 angular
   .module('cinemaAngularJsApp', [
-    'ngMessages',
     'ngRoute'
   ])
   .config(function ($routeProvider) {
@@ -19,11 +18,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
